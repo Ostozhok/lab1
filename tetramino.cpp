417393ff014629b44543290161a76d0fcb24c782
stozhok <sashastozhok@gmail.com>
2023-05-15 18:37:29 +0300

    Initial Commit

---
+++ tetramino.cpp
@@ -0,0 +1,9 @@
+//
+//  tetramino.cpp
+//  lab1
+//
+//  Created by Sasha Stozhok on 15.05.2023.
+//  Copyright © 2023 Sasha Stozhok. All rights reserved.
+//
+
+#include "tetramino.hpp"
---
+++ tetramino.hpp
@@ -0,0 +1,14 @@
+//
+//  tetramino.hpp
+//  lab1
+//
+//  Created by Sasha Stozhok on 15.05.2023.
+//  Copyright © 2023 Sasha Stozhok. All rights reserved.
+//
+
+#ifndef tetramino_hpp
+#define tetramino_hpp
+
+#include <stdio.h>
+
+#endif /* tetramino_hpp */
